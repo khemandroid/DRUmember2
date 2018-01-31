ba7a2fe9abb618c47565af26adf48b94d6b8409b
Dell <in_siamth@hotmail.com>
2018-01-31 16:24:10 +0700

    test 31 jan

--- app/src/main/java/com/thailand/kh/drumember/MainActivity.java
+++ app/src/main/java/com/thailand/kh/drumember/MainActivity.java
@@ -3,11 +3,25 @@ package com.thailand.kh.drumember;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.thailand.kh.drumember.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+
+//            Add Fragment to Activity
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .add(R.id.contentMainFragment, new MainFragment())
+                            .commit();
+        }
+
+
+
     }
 }
